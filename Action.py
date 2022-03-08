bc88b21512ef76d9ec2dea88c224ea28c8ffc0c2
Isaac Dahle <isaacbd.personal@gmail.com>
2022-03-07 18:48:03 -0800

    Added an action class and some notes in it

---
+++ Action.py
@@ -0,0 +1,6 @@
+class Action:
+    def __init__(self, emptyField):
+        self.emptyField= emptyField
+
+    #TODO add handeling of input from the consol and validation
+    #TODO this should handle all of the actions inputted by the player
